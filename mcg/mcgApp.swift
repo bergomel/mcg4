a02ed6d58f7a571853ce1845db2b29624da5a5b0
bergomel <bernardogomel@gmail.com>
2024-06-11 08:17:46 -0300

    Initial Commit

---
+++ mcg/mcgApp.swift
@@ -0,0 +1,17 @@
+//
+//  mcgApp.swift
+//  mcg
+//
+//  Created by Bernardo on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct mcgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
